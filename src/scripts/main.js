cc30b561e6030ca26c707b2cace54e692556c0a3
Vladyslav Tsarenko <vaaaltaaasaaar@gmail.com>
2025-02-03 13:39:17 -0600

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,14 @@
 'use strict';
 
-// write code here
+const listEmployee = [...document.querySelectorAll('li')];
+
+listEmployee.sort(
+  (a, b) =>
+    a.dataset.salary.replace(/\D/g, '') - b.dataset.salary.replace(/\D/g, ''),
+);
+
+const ulEmployee = document.querySelector('ul');
+
+listEmployee.forEach((elem) => {
+  ulEmployee.insertAdjacentElement('afterbegin', elem);
+});
